2c9e160d402535475bef81f69be44cf46525de0f
Luiz Gonzaga <roinuj-01@outlook.com>
2022-08-15 18:34:53 -0300

    Teste com API e Query

---
+++ Spring Boot/repository/src/main/java/com/example/repository/CryptoRepository.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CryptoRepository extends JpaRepository<Crypto, Long>{
+    @Query("select c from Crypto c where c.rate > 1500")
+    List<Crypto> getAllHigh();
+    List<Crypto> findByExchange(String exchange);
+}
