c44449fffc3ab4c6fa06a4d8e0bf85d06346c191
Luiz Gonzaga <roinuj-01@outlook.com>
2022-08-20 19:04:35 -0300

    Security Login with JWT and H2 Database

---
+++ Spring Boot/loginauth/src/main/java/com/example/loginauth/LoginauthApplication.java
@@ -0,0 +1,11 @@
+package com.example.loginauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginauthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LoginauthApplication.class, args);
+	}
+}
---
+++ Spring Boot/loginauth/src/main/java/com/example/loginauth/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.loginauth.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
