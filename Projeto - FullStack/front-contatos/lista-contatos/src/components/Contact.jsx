18845729fd65c609f135b4bc1ee597309878a7c8
Luiz Gonzaga <roinuj-01@outlook.com>
2022-10-14 00:32:24 -0300

    adciona front da lista de contatos

---
+++ Projeto - FullStack/front-contatos/lista-contatos/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = ({id, name}) => {
+    return (
+        <div className="card" key={id}>
+            <p>ID: {id}</p>
+            <p>Nome: {name}</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ Projeto - FullStack/front-contatos/lista-contatos/src/components/Phone.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Phone = ({ddd, ddi, pnumber}) => {
+    return (
+        <div>
+            <p>DDD: {ddd}</p>
+            <p>DDI: {ddi}</p>
+            <p>Numero: {pnumber}</p>
+        </div>
+    )
+}
+export default Phone;
